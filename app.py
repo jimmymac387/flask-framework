a8eb838f0a864db6879be336058993240e0efccb
James MacCarthy <james.maccarthy@gmail.com>
2020-06-25 21:57:54 -0700

    Test form requests

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+alpha_path = "https://www.alphavantage.co/query?"
+function = "function=TIME_SERIES_INTRADAY"
+symbol = "&symbol=IBM"
+interval = "&interval=5min"
+api_key = "&apikey=demo"
 
-@app.route('/')
+alpha_path + function + symbol + interval + api_key
+
+app.vars = {}
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        app.vars["ticker"] = request.form["ticker"]
+        app.vars["feature"] = request.form["feature"]
+        print(app.vars["ticker"])
+        return "Surprise!"
 
 
 @app.route('/about')
